29d85c2db38803378541fdf7c2accc063e23fef4
JJimenez-1 <jjimenez@cifpfbmoll.eu>
2020-05-06 17:42:04 +0200

    feat(ControllerAgendaService): Nueva funcion para poder añadir un contacto con el metodo POST

--- src/main/java/org/formacio/mvc/ControllerAgendaService.java
+++ src/main/java/org/formacio/mvc/ControllerAgendaService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -29,9 +30,15 @@ public class ControllerAgendaService {
 	
 	@RequestMapping(path="/contacte/{id}")
 	@ResponseBody
-	public Persona recuperaContacte(@PathVariable String id){
+	public Persona devolverContacto(@PathVariable String id){
 			return agenda.recupera(id);
 	}
 	
+	@RequestMapping(path="/afegir", method=RequestMethod.POST)
+	@ResponseBody
+	public String afegirContacte(@RequestParam String id, @RequestParam String nom, @RequestParam String telefon){
+		agenda.inserta(id, nom, telefon);
+		return "ok";
+	}
 	
 }
