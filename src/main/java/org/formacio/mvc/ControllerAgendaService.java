ef8ae747866df551635b8483c28a7b20448d1be5
JJimenez-1 <jjimenez@cifpfbmoll.eu>
2020-05-06 16:37:30 +0200

    feat(ControllerAgendaService): Nueva función para devolver el numero de contactos existentes

--- src/main/java/org/formacio/mvc/ControllerAgendaService.java
+++ src/main/java/org/formacio/mvc/ControllerAgendaService.java
@@ -1,9 +1,22 @@
 package org.formacio.mvc;
 
+import org.formacio.repositori.AgendaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ControllerAgendaService {
 
+	@Autowired
+	private AgendaService agenda;
+	
+	@RequestMapping(path="/nombre")
+	@ResponseBody
+	public int numeroContactos() {
+		return agenda.nombreContactes();
+	}
+	
 	
 }
