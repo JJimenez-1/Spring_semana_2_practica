b3aa3589046a829576d9943e584d9c810da48f8b
JJimenez-1 <jjimenez@cifpfbmoll.eu>
2020-05-06 17:18:41 +0200

    feat(ControllerAgendaService): añadir devolverTelefono del contacto

--- src/main/java/org/formacio/mvc/ControllerAgendaService.java
+++ src/main/java/org/formacio/mvc/ControllerAgendaService.java
@@ -4,6 +4,7 @@ import org.formacio.repositori.AgendaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -18,5 +19,9 @@ public class ControllerAgendaService {
 		return agenda.nombreContactes();
 	}
 	
-	
+	@RequestMapping(path="/telefon")
+	@ResponseBody
+	public String devolverTelefono(@RequestParam String id){
+		return agenda.recupera(id).getTelefon();
+	}
 }
