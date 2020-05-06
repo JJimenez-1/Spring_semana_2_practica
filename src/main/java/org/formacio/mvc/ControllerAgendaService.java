0cb521c015f1ee04073a0e830bdb433d4f890073
JJimenez-1 <jjimenez@cifpfbmoll.eu>
2020-05-06 17:30:18 +0200

    feat(ControllerAgendaService): añadir funcion para recuperar contacto

--- src/main/java/org/formacio/mvc/ControllerAgendaService.java
+++ src/main/java/org/formacio/mvc/ControllerAgendaService.java
@@ -1,8 +1,10 @@
 package org.formacio.mvc;
 
 import org.formacio.repositori.AgendaService;
+import org.formacio.repositori.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,4 +26,12 @@ public class ControllerAgendaService {
 	public String devolverTelefono(@RequestParam String id){
 		return agenda.recupera(id).getTelefon();
 	}
+	
+	@RequestMapping(path="/contacte/{id}")
+	@ResponseBody
+	public Persona recuperaContacte(@PathVariable String id){
+			return agenda.recupera(id);
+	}
+	
+	
 }
